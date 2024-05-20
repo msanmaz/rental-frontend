75d5a7d32fab939496a8be095003380cde429be2
Mert Osanmaz <mosanmaz@danusports.com>
2024-05-20 16:25:18 +0100

    vercel build path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, 'src'),
+      };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
